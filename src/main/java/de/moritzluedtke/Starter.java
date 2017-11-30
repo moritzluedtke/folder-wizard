732ae871e89782ef0408ca92d76f3ff0eef5898c
ML-PC\Moritz Lüdtke <moritz.luedtke@gmx.de>
2017-11-30 19:15:43 +0100

    -JAR is now executable and runs by double clicking it

--- src/main/java/de/moritzluedtke/Starter.java
+++ src/main/java/de/moritzluedtke/Starter.java
@@ -17,18 +17,19 @@ public class Starter extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        URL mainWindowUrl = getClass().getClassLoader().getResource("layout/mainWindow.fxml");
+        URL mainWindowUrl = ClassLoader.getSystemResource("layout/MainWindow.fxml");
+//        URL mainWindowUrl = getClass().getResource("/layout/MainWindow.fxml");
 
         createMainWindowGUI(primaryStage, mainWindowUrl).show();
     }
 
     private Stage createMainWindowGUI(Stage primaryStage, URL resource) {
         final int MAIN_WINDOW_WIDTH = 400;
-        final int MAIN_WINDOW_HEIGHT = 300;
+        final int MAIN_WINDOW_HEIGHT = 250;
 
         try {
             Parent root = FXMLLoader.load(resource);
-            primaryStage.setTitle("ServerProgram - by Moritz Lüdtke");
+            primaryStage.setTitle("FileOrganizer - by Moritz Lüdtke");
             primaryStage.setScene(new Scene(root, MAIN_WINDOW_WIDTH, MAIN_WINDOW_HEIGHT));
             primaryStage.setResizable(false);
         } catch (IOException e) {
